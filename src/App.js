b78184267f902005b996b96e85209f2eb715540f
sandeep18 <sandeep.pansari98@gmail.com>
2023-12-30 18:17:52 +0530

    adding tailwind css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 
-import {ChatIcon} from "./svg"
+
 function App() {
   return (
-    <div className="App">
-     <h1>Weilcome whatp</h1>
-     <ChatIcon className="red"/>
+    <div className="dark">
     </div>
   );
 }
